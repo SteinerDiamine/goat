4b4e9aa57d2060e474dcae49e599f77ea7507e8c
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-05-03 10:21:00 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
 import Navbar from "@/components/global/Navbar";
+import { ContainerScroll } from "@/components/global/Containers-scroll-animation"; 
+import { Button } from "@/components/ui/button";
+import { clients, products } from "@/lib/constants";
+import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
+import { HeroParallax } from "@/components/global/connect-parallax";
+import { LampComponent } from "@/components/global/lamp";
+import { CardBody, CardContainer } from "@/components/global/3d-card";
+import { CheckIcon } from "lucide-react";
 
 
 
@@ -6,6 +14,49 @@ export default function Home() {
   return (
     <main >
       <Navbar/>
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center flex-col">
+                <Button
+                  size={'lg'}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work
+                </h1>
+              </div>
+            }
+          />
+        </div>
+      </section>
+      <InfiniteMovingCards
+        className="md:mt-[18rem] mt-[-100px]"
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
+      <section>
+        <HeroParallax products={products}></HeroParallax>
+      </section>
+      <section className="mt-[-500px]">
+    <LampComponent/>
+    <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -m-72">
+      <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-[#E2CBFF] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+             
+              
+            </CardBody>
+
+      </CardContainer>
+    </div>
+      </section>
       
 
       
